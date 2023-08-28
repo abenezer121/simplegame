b42b3d8842a0a48d973adfb60c64848dc5feae92
abenezer121 <webgoat12@gmail.com>
2023-08-28 16:31:26 +0300

    mechanial of the game

--- src/pages/Mainscreen.js
+++ src/pages/Mainscreen.js
@@ -1,68 +1,173 @@
-import React , { useState , useEffect} from "react";
+
+
+     
+
+   
+
+import React , { useState , useEffect,useLayoutEffect,useRef} from "react";
 
 
 
 const GameScreen = () => {
-    return (
-        
-        <div className=" mt-[10%] h-full items-center">
-            <p>Space invasion</p>
-            <GameBoard/>
-        </div>
-    )
+                    return (
+                        <div className=" mt-[10%] h-full items-center">
+                            <p>Space invasion</p>
+                            <GameBoard/>
+                        </div>
+                    )
 }
+        
 const GameBoard = () => {
+                
+                
+                    //  initial coordinates of the text
+                    const [alienX, setX] = useState (0);
+                    const [alienY, setY] = useState (0);
+                
+                    // initial coordinate of the missle
+                    const [missleX , setMissleX] = useState(750)
+                    const [missleY , setMissileY] = useState(450)
+                    const [missileSpeed , setMissileSpeed] = useState(0)
+                    const [missileAcceleration , setMissileAcceleration] = useState(0.2)
+                    // init missle pointing angle
+                    const [missileAngle , setMissileAngle] = useState(0)
+                
+                
+                      // Define the dimensions of the text
+                    const textWidth = 100;
+                    const textHeight = 100;
+                    const friction = 0.05;
+                    const maxSpeed = 5; 
+                  
+                     
+                 
+                    useEffect (() => {
+                
+                                // the dimensions of the div
+                            const divWidth = 800;
+                            const divHeight = 546;
+                        
+                          
+                            // generate random coordinates within the div boundaries
+                            const maxX = divWidth - textWidth;
+                            const maxY = divHeight - textHeight;
+                        
+                            const randomX = Math.floor (Math.random () * maxX);
+                            const randomY = Math.floor (Math.random () * maxY);
+                            
+                          
+                      
+                            // Set the state with the new coordinates
+                            setX (randomX);
+                            setY (randomY);
+                    },[]);
+                
+                
+                        const alienTextStyle = {
+                            width: `${textWidth}px`,
+                            height: `${textHeight}px`,
+                            color : 'white',
+                            position: "absolute",
+                            left: `${alienX}px`,
+                            top: `${alienY}px`,
+                        };
 
- 
-        // the dimensions of the div
-        const divWidth = 800;
-        const divHeight = 546;
-      
-        // Define the dimensions of the text
-        const textWidth = 100;
-        const textHeight = 100;
-      
-        //  initial coordinates of the text
-        const [x, setX] = useState (0);
-        const [y, setY] = useState (0);
-             
-        const textStyle = {
-            width: `${textWidth}px`,
-            height: `${textHeight}px`,
-            color : 'white',
-            position: "absolute",
-            left: `${x}px`,
-            top: `${y}px`,
-        };
-       
-        useEffect (() => {
-            // generate random coordinates within the div boundaries
-            const maxX = divWidth - textWidth;
-            const maxY = divHeight - textHeight;
-        
-            const randomX = Math.floor (Math.random () * maxX);
-            const randomY = Math.floor (Math.random () * maxY);
-            
-          
-      
-            // Set the state with the new coordinates
-            setX (randomX);
-            setY (randomY);
-        },[]);
-    return (
-        <div className=" mt-[5%] mx-auto w-[95%] h-[75%] bg-black" style={{position: "relative"}}>
-         <p style={textStyle}>Hello world</p>
-        </div>
-    )
-}
 
-const MainScreen = ()=> {
-    return (   
-            <div class="flex h-full">
-                <div class="flex-1 bg-red-500">Left</div>
-                <div class="flex-1 h-full w-full"><GameScreen/></div>
-            </div>
-            )
-}
+                        const missileTextStyle = {
+                            width: `${textWidth}px`,
+                            height: `${textHeight}px`,
+                            color : 'white',
+                            position: "absolute",
+                            left: `${missleX}px`,
+                            top: `${missleY}px`,
+                            transform: `rotate(${missileAngle}deg)`,
+                        };
+                
+                        
+                
+                   
+                 
+                 
+                        // calculate the angle and new posiion of the missile
+                        const calculateAngle = (x,y,direction) => {
+                 
+                            let newspeed = missileSpeed;
+                            let angle = missileAngle
+                          
+                             if (direction === "ArrowUp") {
+                                newspeed = ( missileSpeed+ missileAcceleration);
+                             
+                            } 
+                
+                           
+                             if(newspeed > maxSpeed) newspeed = maxSpeed
+                           
+                            if(missileSpeed > 0)  newspeed = newspeed - friction   
+                            
+                            if(newspeed < friction) newspeed = 0
+                          
+                            if (direction === "ArrowLeft") angle = missileAngle - 0.5;
+                             
+                            else if (direction === "ArrowRight") angle = missileAngle + 0.5;
+                          
+                            // Convert missileAngle from degrees to radians
+                            const angleInRadians = missileAngle * (Math.PI / 180);
 
-export default MainScreen;
+
+                           // Calculate new positions using radians
+                            let newY = y - Math.cos(angleInRadians) * newspeed;
+                            let newX = x + Math.sin(angleInRadians) * newspeed;
+
+                           
+                           
+                            setMissileSpeed(newspeed)
+                            setMissileAngle(angle)
+                           
+                            
+                            return [newX, newY];           
+                        }
+                        const keyDownEvent = (event) => {
+                     
+                       
+                      
+                          if (event.code === "ArrowUp") {
+                     
+                            let newXY = calculateAngle(missleX , missleY , event.code)
+                            setMissleX(() => newXY[0] );
+                            setMissileY(() =>  newXY[1] )
+                           
+                        }
+                
+                
+                
+                        if (event.code === "ArrowRight") { 
+                            let newXY = calculateAngle(missleX , missleY , event.code)
+                            setMissleX(() => newXY[0] );
+                            setMissileY(() =>  newXY[1] )
+                        }
+                        
+                        if (event.code === "ArrowLeft") { 
+                            let newXY = calculateAngle(missleX , missleY , event.code)
+                            setMissleX(() => newXY[0] );
+                            setMissileY(() =>  newXY[1] ) 
+                        }
+                        };
+                
+                    return (
+                        <div className=" mt-[5%] mx-auto w-[95%] h-[75%] bg-black" style={{position: "relative"}} onKeyDown={keyDownEvent} tabIndex={0}>
+                         <p style={alienTextStyle}>ship</p> {/* the alien ship*/}
+                         <p style={missileTextStyle}>|</p> {/* the missile*/}
+                        </div>
+                    )
+                }
+                
+                const MainScreen = ()=> {
+                    return (   
+                            <div class="flex h-full" >
+                                <div class="flex-1 bg-red-500">Left</div>
+                                <div class="flex-1 h-full w-full"><GameScreen/></div>
+                            </div>
+                            )
+                }
+                
+                export default MainScreen;
