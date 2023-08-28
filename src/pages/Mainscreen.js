1a264f27188e0d1c122fe8590d55c34239e6848e
abenezer121 <webgoat12@gmail.com>
2023-08-28 10:43:46 +0300

    randomly generate tex nsidehe blackbox

--- src/pages/Mainscreen.js
+++ src/pages/Mainscreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , { useState , useEffect} from "react";
 
 
 
@@ -12,9 +12,46 @@ const GameScreen = () => {
     )
 }
 const GameBoard = () => {
+
+ 
+        // the dimensions of the div
+        const divWidth = 800;
+        const divHeight = 546;
+      
+        // Define the dimensions of the text
+        const textWidth = 100;
+        const textHeight = 100;
+      
+        //  initial coordinates of the text
+        const [x, setX] = useState (0);
+        const [y, setY] = useState (0);
+             
+        const textStyle = {
+            width: `${textWidth}px`,
+            height: `${textHeight}px`,
+            color : 'white',
+            position: "absolute",
+            left: `${x}px`,
+            top: `${y}px`,
+        };
+       
+        useEffect (() => {
+            // generate random coordinates within the div boundaries
+            const maxX = divWidth - textWidth;
+            const maxY = divHeight - textHeight;
+        
+            const randomX = Math.floor (Math.random () * maxX);
+            const randomY = Math.floor (Math.random () * maxY);
+            
+          
+      
+            // Set the state with the new coordinates
+            setX (randomX);
+            setY (randomY);
+        },[]);
     return (
-        <div className=" mt-[5%] mx-auto w-[95%] h-[75%] bg-black">
-            <p>asdf</p>
+        <div className=" mt-[5%] mx-auto w-[95%] h-[75%] bg-black" style={{position: "relative"}}>
+         <p style={textStyle}>Hello world</p>
         </div>
     )
 }
